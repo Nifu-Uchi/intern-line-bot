3a7516566df549adfd2127de6aaf645de3ca0624
hirex0609 <37492158+hirex0609@users.noreply.github.com>
2020-02-28 14:56:41 +0900

    Level2　two light control.aicon/tv control. add some routine. add error message pattern. get room-env status function

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'bigdecimal'
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
-
+  APPLIANCE_ID_LIST = {'リビング'=>ENV["LIVINGLIGHT_ID"],'寝室'=>ENV["BROOMLIGHT_ID"],'エアコン'=>ENV["AIRCON_ID"],'テレビ'=>ENV["TV_ID"]}
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
@@ -13,56 +13,138 @@ class WebhookController < ApplicationController
   end
 
   def message_convert(msg)
+    puts('User:'+msg)
     result = msg.split("、")
     buttonlist = {"オン"=>'on','オフ'=>'off'}
     result[1] = buttonlist[result[1]]
     return result
   end
   
-#送られたメッセージの判定
-  def call_api(appliance,order)
 
+  def call_api(appliance,order)
     case appliance
-    when '気温は？' ,
-      action_response = gettemp
-    when '寒くない？'　 
-      nowtemp =gettemp
+    when '気温は？'
+      ans = gettemp
+      additional_massage = 'にゃ'
+      action_response = ("今の室温は" + ans.to_s + "度くらい" + additional_massage)
+    when '寒くない？'
+      nowtemp =gettemp.to_f
       if nowtemp > 14 then
+        action_response = ('今は' + nowtemp.to_s + "度で，寒くはないよー．\nありがとね.")
+      else 
+        action_response = ('今は' + nowtemp.to_s + "度で,ちょっと寒いかも..暖房つけてよ.")
       end
-    #エアコンのオンオフをしたときにgettempして，それに応じたメッセージを返す
+    when '家をチェックして！'
+      answerset = get_appliances_status
+      temp = gettemp.to_s
+      action_response = "...はーーーーい.\nえーっと...\nいまの室温は"+temp+"度\n"+
+                        'リビングの電気は'+answerset['livingroomlight_state']+"になってて,\n"+
+                        '寝室の電気は' +answerset['bedroomlight_state']+"\n"+
+                        'あと，エアコンは' + answerset['aircon_state'] + "かなぁ\n"+
+                        "もう二度とやらせないでほしいにゃ"
     when 'リビング','寝室'
       action_response = light(appliance,order)
-    when '全灯'
+    when '電気全部つけて'
       light('リビング','on')
       light('寝室','on')
       action_response = '全部つけたよ'
+    when '電気全部消して'
+      light('リビング','off')
+      light('寝室','off')
+      action_response = '全部消したよ'
+    when 'みんなを起こして！'
+      light('リビング','on')
+      light('寝室','on')
+      tv('テレビ','on')
+      tv('エアコン','on')
+      action_response = '全部つけた！！！みんな起きてにゃ'
+    when '節約してよ'
+      light('リビング','off')
+      light('寝室','off')
+      aircon('エアコン','オフ')
+      action_response = 'はーい...全部消しましたニャ...'
+    when 'エアコン'
+      action_response = aircon(appliance,order)
+    when 'テレビ'
+      action_response = tv(appliance,order)
     else
-      action_response = 'ごめん，わからない...'
+      err_respon = ['なに？','今寝てるからさ...','おやつの話？','靴下かじっていい？','さっきおやつ箱開けちゃった...']
+      random = Random.new
+      action_response = err_respon[random.rand(0..(err_respon).size)]
     end
     return action_response
   end
-#送られたメッセージの判定ここまで############
-def light(lightname,button)
-  puts(button)
-  #puts('ライト関数')
-  #puts(lightname)
+
+##家電操作関数（POST）
+  def aircon(appliance,button)
   if button=='N' then
     return '何を押せばいいの？'
+  elsif button == 'on'
+    aircon_button = ''
+  elsif button = 'off'
+    aircon_button = 'power-off'
   end
-  appliance_id_list = {'リビング'=>ENV["LIVINGLIGHT_ID"],'寝室'=>ENV["BROOMLIGHT_ID"]}
-  appliance_id = appliance_id_list.dig(lightname)
+  appliance_id = APPLIANCE_ID_LIST[appliance]
   if appliance_id.nil? then
     return 'どれを動かせばいいかわからなくなっちゃった'
   end
-  url = 'https://api.nature.global/1/appliances/'+appliance_id+'/light?button='+button
-  puts (url)
+  url = 'https://api.nature.global/1/appliances/'+appliance_id+'/aircon_settings?button='+aircon_button
   summary = apipost(url)
-  puts(summary)
-  responsemsg = lightname+'ライトを'+button+'にしたよ'
-  puts(responsemsg)
+  responsemsg = 'エアコンを'+button+'にしたよ'
   return responsemsg
-end
-##API関数ここから############
+  end
+
+  def light(appliance,button)
+    if button=='N' then
+      return '何を押せばいいの？'
+    end
+    appliance_id = APPLIANCE_ID_LIST[appliance]
+    if appliance_id.nil? then
+      return 'どれを動かせばいいかわからなくなっちゃった'
+    end
+    url = 'https://api.nature.global/1/appliances/'+appliance_id+'/light?button='+button
+    summary = apipost(url)
+    responsemsg = appliance+'ライトを'+button+'にしたよ'
+  end
+  def tv(appliance,button)
+    if button=='N' then
+      return '何を押せばいいの？'
+    end
+    appliance_id = APPLIANCE_ID_LIST[appliance]
+    puts(appliance)
+    puts(APPLIANCE_ID_LIST)
+    puts(ENV)
+    if appliance_id.nil? then
+      return 'どれを動かせばいいかわからなくなっちゃった'
+    end
+    url = 'https://api.nature.global/1/appliances/'+appliance_id+'/tv?button=power'##テレビはON/OFF区別がない
+    summary = tvpost(url)
+    responsemsg = 'テレビの電源ボタンを押しといたよ'
+  end
+
+##状態取得関数（GET)
+  def gettemp #気温取る
+    hash = apiget('https://api.nature.global/1/devices')
+    tempr = hash.dig(0,'newest_events','te','val')
+    answer = tempr.to_f.round(3).to_s
+    return answer
+  end
+
+  def get_appliances_status
+  hash = apiget('https://api.nature.global/1/appliances')
+  state_convert = {'on': 'つけっぱ','off': 'きえてる','power-off': 'きえてる','': 'ついてる'}
+  appliance_state ={}
+  appliance_state['bedroomlight_state'] = hash.dig(0,'light','state','power')
+  appliance_state['livingroomlight_state'] = hash.dig(2,'light','state','power')
+  if (hash.dig(1,'settings','button')).nil? then
+    appliance_state['aircon_state'] = 'たぶんついてる'
+  else
+    appliance_state['aircon_state'] = '消えてる'
+  end
+  return appliance_state
+  end
+
+##api get/post
   def apiget(url)
     key = ENV["REMO_KEY"]
     uri = URI.parse(url)
@@ -75,9 +157,8 @@ end
     hash = JSON.parse(res.body)
     return hash
   end
-
   def apipost(url)
-    remoanser = 'N'
+    remoanswer = 'N'
     key = ENV["REMO_KEY"]
     uri = URI.parse(url)
     puts(uri)
@@ -91,15 +172,21 @@ end
     
     return hash
   end
-
-  def gettemp #気温取る
-    hash = apiget('https://api.nature.global/1/devices')
-    tempr = hash.dig(0,'newest_events','te','val')
-    anser = ('現在の室温は' + tempr.to_f.round(3).to_s + '度だよ．')
-    return anser
+  def tvpost(url)
+    remoanswer = 'N'
+    key = ENV["REMO_KEY"]
+    uri = URI.parse(url)
+    puts(uri)
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["Content-Type"] = 'application/x-www-form-urlencoded'
+    req["Authorization"] = 'Bearer '+key
+    req["Accept"] = 'application/json'
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    res = https.request(req)
+    hash = JSON.parse(res.body)
+    return hash
   end
-##API関数ここまで############
-
 
   def callback
     body = request.body.read
@@ -116,6 +203,7 @@ end
         when Line::Bot::Event::MessageType::Text
           appliance,order = message_convert(event.message['text'])
           cat_response = call_api(appliance,order)
+          puts (cat_response)
           message = {
             type: 'text',
             text: cat_response
